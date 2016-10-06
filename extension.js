41de9fb1134baf6ae724724aa2fca427652b5520
RTrave <roman.trave@abadcafe.org>
2016-10-06 10:37:59 +0200

    Typo fixes.

--- extension.js
+++ extension.js
@@ -13,18 +13,15 @@ let launcher_mumble;
 
 function enable() {
     origin = Aim.prototype._redisplay;
-
     if(GLib.spawn_command_line_sync("which mumble")[3] === 0) {
-        launcher_mumble = "Mumble-Overlay"
+        launcher_mumble = "Mumble-Overlay";
     } else {
         Main.notifyError("mumble-overlay", "Error: Mumble is not installed");
-
         return;
     }
 
     Aim.prototype._redisplay = function () {
         origin.call(this, arguments);
-
         this._mumble = new PopupMenu.PopupMenuItem(_(launcher_mumble));
         this.addMenuItem(this._mumble, this._getMenuItems()
             .indexOf(this._newWindowMenuItem) + 1);
@@ -32,11 +29,9 @@ function enable() {
             if(this._source.app.state == Shell.AppState.STOPPED) {
                 this._source.animateLaunch();
             }
-
             Util.spawnApp([launcher_mumble.toLowerCase(), _getCommand(this._source.app.get_id())]);
             this.emit('activate-window', null);
         }));
-
     }
 }
 
@@ -49,7 +44,6 @@ function _getCommand(file) {
         try {
             let content = GLib.file_get_contents(extensionLocations[i] + "/share/applications/" + file)[1];
             let line = /Exec=.+/.exec(content)[0];
-
             return line.substr(5);
         } catch(error) {
             log(error);
